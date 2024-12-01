bdd0057d8efd21c53ca80424a09e634bbeb196c3
CH Varun <130673937+TheCoder30ec4@users.noreply.github.com>
2024-12-01 08:21:01 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,68 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu');
+
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+};
+
+let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-sun');
+
+    if(themeBtn.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+
+};
+
+var swiper = new Swiper(".gallery-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop:true,
+    autoplay:{
+      delay: 3000,
+      disableOnInteraction:false,
+    }
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    grabCursor: true,
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+
+AOS.init({
+  duration: 1200,          // Smooth transition duration
+  delay: 300,             // Delay for the animation to start
+  easing: 'ease-in-out',  // Smoother easing
+  once: true,             // Make sure the animation only triggers once
+  anchorPlacement: 'top-bottom', // Trigger animations when elements are fully visible
+  disable: 'mobile',  
+});
